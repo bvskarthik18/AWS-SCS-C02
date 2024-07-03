98ba65e158064344757412a40dc54f3ebe384530
bvskarthik18 <bvskarthik18@gmail.com>
2024-07-03 19:46:06 +0530

    Deleted RetrieveSecrets.py

--- RetrieveSecrets.py
+++
@@ -1,8 +0,0 @@
-secrets_extension_endpoint = "http://localhost:" + \
-    secrets_extension_http_port + \
-    "/secretsmanager/get?secretId=" + \
-    <secret_name>
-  
-  r = requests.get(secrets_extension_endpoint, headers=headers)
-  
-  secret = json.loads(r.text)["SecretString"] # load the Secrets Manager response into a Python dictionary, access the secret
